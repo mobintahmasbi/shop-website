05d77a7301b06855b926a3db4e95fb5da5f01c44
mobin <mobintahmasbi13801202@gmail.com>
2023-08-02 01:57:17 +0430

    add first step of error handling for login page

--- front-end/MLindex.js
+++ front-end/MLindex.js
@@ -4,35 +4,57 @@ const phoneNumber = document.getElementById("phoneNumberInput");
 const loginButton = document.getElementById("loginButton");
 
 function checkInputs(element) {
-    let mailiciousKeyWord = ["'", "-", "=", "+", ";", '"', '%', "#", ".", ","]
-    let input = element.value
-    let bol = true
-    mailiciousKeyWord.forEach((word) => {
-        if (input.includes(word)){
-            bol = false
-        }
-    })
-    return bol
-
+  let mailiciousKeyWord = ["'", "-", "=", "+", ";", '"', "%", "#", ".", ","];
+  let input = element.value;
+  let bol = true;
+  mailiciousKeyWord.forEach((word) => {
+    if (input.includes(word)) {
+      bol = false;
+    }
+  });
+  return bol;
 }
 
 function validate() {
-  let counter = 0
+  let counter = 0;
+  let errors = [];
   if (userName.value.length >= 8 && userName.value.length < 25) {
-    if(checkInputs(userName)){
-      counter++
+    if (checkInputs(userName)) {
+      counter++;
+    } else {
+      errors.push([userName, "لطفا از علامت های غیر معمول استفاده نکنید"]);
     }
+  } else {
+    errors.push([
+      userName,
+      "تعداد کاراکتر های مورد استفاده باید بین 8 الی 25 کلمه باشد",
+    ]);
   }
   if (password.value.length >= 8 && password.value.length < 25) {
-    if(checkInputs(password)){
-      counter++
+    if (checkInputs(password)) {
+      counter++;
+    } else {
+      errors.push([password, "لطفا از علامت های غیر معمول استفاده نکنید"]);
     }
+  } else {
+    errors.push([
+      password,
+      "تعداد کاراکتر های مورد استفاده باید بین 8 الی 25 کلمه باشد",
+    ]);
   }
   if (phoneNumber.value.length === 11) {
-    if(checkInputs(phoneNumber)){
-      counter++
+    if (checkInputs(phoneNumber)) {
+      counter++;
+    } else {
+      errors.push([phoneNumber, "لطفا از علامت های غیر معمول استفاده نکنید"]);
     }
+  } else {
+    errors.push([
+      phoneNumber,
+      "شماره تلفن باید دارای 11 شماره باشد",
+    ]);
   }
+  console.log(errors);
   return 3 === counter;
 }
 
