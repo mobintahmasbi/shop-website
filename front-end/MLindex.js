d79766634c57a78020ad751d67593c6c66e8f47f
mobin <mobintahmasbi13801202@gmail.com>
2023-08-18 19:26:37 +0430

    complete the ui logic withOut connecting to database

--- front-end/MLindex.js
+++ front-end/MLindex.js
@@ -1,8 +1,13 @@
+// input elements are selected here
 const userName = document.getElementById("userNameInput");
 const password = document.getElementById("passwordInput");
-const phoneNumber = document.getElementById("phoneNumberInput");
+const email = document.getElementById("emailInput");
+const confirmCode = document.getElementById("confirmCodeInput");
+// button elements are selected here
+const sendingCodeButton = document.getElementById("sendingCodeButton");
 const loginButton = document.getElementById("loginButton");
 
+// check inputs elements for preventing sql injection
 function checkInputs(element) {
   let mailiciousKeyWord = ["'", "-", "=", "+", ";", '"', "%", "#", ".", ","];
   let input = element.value;
@@ -16,30 +21,32 @@ function checkInputs(element) {
 }
 
 userName.addEventListener("blur", () => {
-  clearspecificinvalidfeedback(userName, 'userName')
-})
+  clearspecificinvalidfeedback(userName, "userName");
+});
 
 password.addEventListener("blur", () => {
-  clearspecificinvalidfeedback(password, 'password')
-})
+  clearspecificinvalidfeedback(password, "password");
+});
 
-phoneNumber.addEventListener("blur", () => {
-  clearspecificinvalidfeedback(phoneNumber, 'phoneNumber')
-})
+email.addEventListener("blur", () => {
+  clearspecificinvalidfeedback(email, "email");
+});
 
+//check a specific input value for checking validity if it's valid then clear the error message
 function clearspecificinvalidfeedback(el, elName) {
-  let validation = validate()
+  let validation = validate();
   let bol = true;
   validation.forEach((v) => {
     if (v[2] === elName) {
       bol = false;
     }
-  })
+  });
   if (bol) {
-    el.nextElementSibling.style.display = 'none'
+    el.nextElementSibling.style.display = "none";
   }
 }
 
+// vaalidate the length of the inputs value and call checkInputs function for every input
 function validate() {
   let counter = 0;
   let errors = [];
@@ -47,59 +54,81 @@ function validate() {
     if (checkInputs(userName)) {
       counter++;
     } else {
-      errors.push([userName, "لطفا از علامت های غیر معمول استفاده نکنید", 'userName']);
+      errors.push([
+        userName,
+        "لطفا از علامت های غیر معمول استفاده نکنید",
+        "userName",
+      ]);
     }
   } else {
     errors.push([
       userName,
       "تعداد کاراکتر های مورد استفاده باید بین 8 الی 25 کلمه باشد",
-      'userName'
+      "userName",
     ]);
   }
   if (password.value.length >= 8 && password.value.length < 25) {
     if (checkInputs(password)) {
       counter++;
     } else {
-      errors.push([password, "لطفا از علامت های غیر معمول استفاده نکنید", 'password']);
+      errors.push([
+        password,
+        "لطفا از علامت های غیر معمول استفاده نکنید",
+        "password",
+      ]);
     }
   } else {
     errors.push([
       password,
       "تعداد کاراکتر های مورد استفاده باید بین 8 الی 25 کلمه باشد",
-      'password'
+      "password",
     ]);
   }
-  if (phoneNumber.value.length === 11) {
-    if (checkInputs(phoneNumber)) {
+  if (email.value.length >= 8) {
+    if (checkInputs(email)) {
       counter++;
     } else {
-      errors.push([phoneNumber, "لطفا از علامت های غیر معمول استفاده نکنید", 'phoneNumber']);
+      errors.push([
+        email,
+        "لطفا از علامت های غیر معمول استفاده نکنید",
+        "email",
+      ]);
     }
   } else {
     errors.push([
-      phoneNumber,
-      "شماره تلفن باید دارای 11 شماره باشد",
-      'phoneNumber'
+      email,
+      "تعداد کاراکتر های مورد استفاده باید بین 8 الی 25 کلمه باشد",
+      "email",
     ]);
   }
-  
+
   return errors;
 }
 
-function clearinvalidfeedbacks(){
+function clearinvalidfeedbacks() {
   userName.nextElementSibling.style.display = "none";
   password.nextElementSibling.style.display = "none";
-  phoneNumber.nextElementSibling.style.display = "none";
+  email.nextElementSibling.style.display = "none";
 }
 
-loginButton.addEventListener("click", (e) => {
-  clearinvalidfeedbacks()
-  let validations = validate()
+function replaceElementClassname(els, removableClassname, newClassname) {
+  els.forEach((el) => {
+    el.parentElement.classList.remove(removableClassname);
+    el.parentElement.classList.add(newClassname);
+  });
+}
+
+sendingCodeButton.addEventListener("click", (e) => {
+  clearinvalidfeedbacks();
+  let validations = validate();
   if (validations.length > 0) {
     validations.forEach((v) => {
-      let el = v[0].nextElementSibling
-      el.innerText = v[1]
-      el.style.display = "flex"
-    })
+      let el = v[0].nextElementSibling;
+      el.innerText = v[1];
+      el.style.display = "flex";
+    });
+  } else {
+    replaceElementClassname([password, email, userName, sendingCodeButton], "d-flex", "d-none")
+    replaceElementClassname([confirmCode, loginButton], "d-none", "d-flex")
   }
 });
